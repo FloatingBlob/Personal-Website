609a4ba5e6af788f950e8aadf0bde1d19c7606bc
Laziness <78thunderstorm@gmail.com>
2024-07-04 23:26:50 +0100

    navbar icon and theme styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
-import Icon from "../assets/icon.png";
 import "./Navbar.css";
+import { SiSpotify, SiSteam } from "react-icons/si";
+import Icon from "../assets/icon.png";
 import Toggle from "./Toggle.js";
 
 export default function NavBar ({ setDarkMode, darkMode }) {
@@ -9,11 +10,17 @@ export default function NavBar ({ setDarkMode, darkMode }) {
             <ul>
                 <li>
                     <a href="/">
-                        <img src={Icon} alt="Icon" />
+                        <img src={Icon} alt="Floor of philosophy icon" />
                     </a>
                 </li>
                 <li>
-                    <Toggle setDarkMode={setDarkMode} darkMode={darkMode} />
+                    <SiSpotify className="links"/>
+                </li>
+                <li> 
+                    <SiSteam className="links"/>
+                </li>
+                <li>
+                    <Toggle setDarkMode={setDarkMode} darkMode={darkMode} className="links" />
                 </li>
             </ul>
         </nav>
