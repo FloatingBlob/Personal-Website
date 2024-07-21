e20762c4ca802cac1ab4c11beb65a2c1c2e61e6c
Laziness <78thunderstorm@gmail.com>
2024-07-21 20:26:47 +0100

    update second page, global variables, fix mobile issues

--- src/components/about/noon.js
+++ src/components/about/noon.js
@@ -10,13 +10,22 @@ export default function About() {
             </div>
             <div className="right">
                 <div>
-                    <p>Greetings.</p>
-                    <p>Welcome to my website</p>
+                    <p className="title">More <span className="highlight">about</span> me</p>
                     <br></br>
-                    <p>You've <span className="highlight">returned</span>.</p>
-                    <p className="indent-text">I'm <span className="highlight">Laziness</span></p>
+                    <p>My name is <span className="highlight">joe</span> and I'm <span className="highlight">78</span> years old.</p>
+                    <p>I live in <span className="highlight">Djibouti City</span> located in <span className="highlight">Djibouti</span>.</p>
                     <br></br>
-                    <p>also known as...</p>
+                    <p>I like to think myself as:</p>
+                    <p className="indent-text">- An <span className="highlight">indie</span> video game enjoyer</p>
+                    <p className="indent-text">- A <del>good</del> programmer</p>
+                    <p className="indent-text">- Lord of <span className="highlight">Toast</span></p>
+                    <br></br>
+                    <p>Favourite Quote:</p> 
+                    {/* Note: Make it so that after a refresh of the page, a random quote is placed here out of a selection. */}
+                    <p className="indent-text">It was good, though. Exquisite, even.</p>
+                    <p className="indent-text">No wonder humans used to eat them... </p>
+                    <br></br>
+                    <p>1-0 Mackerel</p>
                 </div>
             </div>
         </div> 
